5114ffe796ab0d37e363879a8c4e3f0b81535758
Fiona Tay <me@fionatay.com>
2024-05-05 23:58:54 +0800

    Implement API call and render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,57 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client'
 
-export default function Home() {
+import "./styles.css";
+import { useState, useEffect } from "react";
+import React from "react";
+
+const Loading = () => {
+  return <div className="loading">Loading…</div>;
+};
+
+const Codeword = ({ codeword }) => {
+  const DELAY_MS = 500; // half a second
+  const [index, setIndex] = useState(0); // show nothing at first
+
+  useEffect(() => {
+    if (index < codeword.length) {
+      const timeoutId = setTimeout(() => {
+        // console.log(`Setting ${index + 1}`)
+        setIndex(index + 1);
+      }, DELAY_MS);
+    }
+  }, [index]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <ul className="codeword">
+        {codeword
+            .substring(0, index)
+            .split("")
+            .map((char: string, i: number) => (
+                <li className="codeword-char" key={i}>
+                  {char}
+                </li>
+            ))}
+      </ul>
+  );
+};
+
+export default function App() {
+  const API_URL =
+      "https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/626f6e";
+  const [codeword, setCodeword] = useState();
+
+  useEffect(() => {
+    fetchCodeword().then((body: string) => setCodeword(body));
+  }, []);
+
+  const fetchCodeword = async (): Promise<string> => {
+    const response = await fetch(API_URL);
+    return response.text();
+  };
+
+  return (
+      <div className="App">
+        {codeword ? <Codeword codeword={codeword} /> : <Loading />}
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
 }
